e618fcc9dcd0ea5c08bb8d2a93d072b488ec0ce8
Camilla Kolby Thomsen <camillakthomsen@hotmail.com>
2026-04-18 16:17:40 +0200

    update again

--- src/App.js
+++ src/App.js
@@ -3,17 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import Home from "./components/Home";
-import Profile from "./components/Profile";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Forsiden med hjul + to-do liste */}
         <Route path="/" element={<Home />} />
-
-        {/* Profil-siden (uden css lige nu) */}
-        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
