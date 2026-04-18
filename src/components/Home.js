2c017f8e52230f0a6c7364af1bd55996593f2e4c
Camilla Kolby Thomsen <camillakthomsen@hotmail.com>
2026-04-18 15:54:56 +0200

    first update

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import "../style/Home.css";
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h1 className="website-url">
+          www.glow-up.dk
+      </h1>
+    </div>
+  );
+}
